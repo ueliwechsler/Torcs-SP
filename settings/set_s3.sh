8c719315b3f81cef46797004321ab854f2a47d6d
ueli.wechsler <ueli.wechsler@outlook.com>
2019-01-08 21:57:26 +0100

    lukas drive

---
+++ settings/set_s3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Copy quickrace.xml
+cp -f  /home/wueli/Torcs-SP/settings/S3/config/raceman/quickrace.xml /home/wueli/.torcs/config/raceman/quickrace.xml
+# Copy human player config
+cp -f  /home/wueli/Torcs-SP/settings/S3/drivers/human/human.xml /home/wueli/.torcs/drivers/human/human.xml
+# Copy steering config
+cp -f  /home/wueli/Torcs-SP/settings/S3/drivers/human/preferences.xml /home/wueli/.torcs/drivers/human/preferences.xml
+# Screen Setup
+cp -f /home/wueli/Torcs-SP/settings/S3/config/screen.xml /home/wueli/.torcs/config/screen.xml
